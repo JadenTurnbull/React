6ffa08a326c563c7e8b5942d9a48811d9cbdba96
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-05-20 11:06:30 +0200

    proj1 ch-6

--- Projects/Beginning React/beginner_react/src/App.js
+++ Projects/Beginning React/beginner_react/src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
-import Products from './Products' ;
-import {Button} from 'react-bootstrap';
-import Rating from './Rating';
+import JumboTronComponent from './JumboTronComponent';
+import UserForm from './UserForm';
+
 
 class App extends Component {
-
-  formatName (user) {
-    return user.firstName + ' ' + user.LastName;
-  }
-
-    render() {
-      const isValid = true;
-      const products = ["Learning React", "Pro React", "Beginning React"];
-      const listProducts = products.map((product) =>
-      <li key={product.toString()}>{product}</li>
-      );
-
-      return (
-        <div>
-          <Products />
-          <ul>{listProducts}</ul>
-          <Button variant="primary" disabled={isValid}>Default</Button>
-          <Rating rating="1"/>
-          <Rating rating="2"/>
-          <Rating rating="3"/>
-          <Rating rating="4"/>
-          <Rating rating="5"/>
-        </div>
-      );
+  render() {        
+    return (
+      <div>
+        <UserForm />      
+      </div>
+    );
   }
 }
 
