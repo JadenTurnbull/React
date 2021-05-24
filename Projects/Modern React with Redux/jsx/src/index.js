56b809ec8a2e0a4c9f115948bf28ffe29a1e71ae
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-05-24 14:13:57 +0200

    chapter 2

--- Projects/Modern React with Redux/jsx/src/index.js
+++ Projects/Modern React with Redux/jsx/src/index.js
@@ -2,12 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create a react components
 const App = () => {
-    return <div> Hi there!</div>;
+    const buttonText = { text: 'Continue' };
+    const labelText = 'Enter name: '
+    
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'orange', color: 'black'}}>
+              {buttonText.text}  
+            </button>
+        </div>
+    );
 };
 
-// Take the React component and shoiw on screen
+// Take the React component and show on screen
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
