a7e88975df15aeb71aef77e492dfc63f7ccbb733
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-06-15 11:40:37 +0200

    chapter 11

--- Projects/Modern React with Redux/videos/src/components/App.js
+++ Projects/Modern React with Redux/videos/src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-
-const KEY = 'AIzaSyB34VjZHZ15TP4SHBJdhcc0SMXPte7bem0';
+const KEY = 'AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('news')
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -19,14 +23,35 @@ class App extends React.Component {
         key: KEY,
       },
     });
-    this.setState({ videos: response.data.items });
+
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0] 
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
