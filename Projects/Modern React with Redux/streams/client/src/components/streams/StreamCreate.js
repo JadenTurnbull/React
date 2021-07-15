c55a32c2cea97bab50d34f6a02d565cc715f9b94
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-07-15 13:11:43 +0200

    chap 23

--- Projects/Modern React with Redux/streams/client/src/components/streams/StreamCreate.js
+++ Projects/Modern React with Redux/streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderError({ error, touched}) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
 
-export default StreamCreate;
+  renderInput = ({ input, label, meta }) => {
+      const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off"/>
+        {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues)
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <form 
+        onSubmit={this.props.handleSubmit(this.onSubmit)} 
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="descrpition" 
+          component={this.renderInput} 
+          label="Enter Description" 
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'Title required';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Description required'
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);
--- Projects/Modern React with Redux/streams/client/src/index.js
+++ Projects/Modern React with Redux/streams/client/src/index.js
@@ -6,15 +6,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX__DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware())
-    );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider> ,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
